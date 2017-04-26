0120437aaa72ae6256ccf794045135cc707a7ee5
Firman Alhadiansyah <fearman.al@gmail.com>
2017-04-26 09:55:18 +0700

    Fix config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sipuma2/';
-$config['site_url'] = 'http://localhost/sipuma2/';
+$config['base_url'] = 'http://localhost/sipuma/';
+$config['site_url'] = 'http://localhost/sipuma/';
 
 /*
 |--------------------------------------------------------------------------
